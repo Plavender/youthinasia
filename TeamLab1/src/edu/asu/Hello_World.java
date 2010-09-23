a0c1f9246bf4b970e08981a2626494c0d7dd5807
Christopher McDonald <chrismcdonalds7@hotmail.com>
2010-09-22 17:53:48 -0700

    Work please?

---
+++ TeamLab1/src/edu/asu/Hello_World.java
@@ -0,0 +1,16 @@
+package edu.asu;
+
+public class Hello_World {
+	
+ /**
+   * This is our little introductory Git program.
+   * 
+   * @param args - Taking in the console input I guess.
+   * @author Christopher McDonald, chrismcdonalds7@hotmail.com
+   */
+	
+	public static void main(String[] args) {
+	System.out.println("Hello, team!");
+	System.out.println("My name is Christopher McDonald.");
+	}
+}
